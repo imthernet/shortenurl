fc2881ef185db4504a1dd583a14dafea924b8c9c
fafa194 <51638888+fafa194@users.noreply.github.com>
2024-04-06 22:11:07 +0200

    MIcroservices

---
+++ src/main/java/com/example/wojtula/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.wojtula;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ShortUrlGenerator shortUrlGenerator() {
+        return new ShortUrlGenerator();
+    }
+}
