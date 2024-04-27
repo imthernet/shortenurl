c4dac816667f9093fed46c6bc19950d5ede6cbd2
Vvojtek <39921871+imthernet@users.noreply.github.com>
2024-04-27 16:30:09 +0200

    Delete build directory

--- build/resources/main/schema.sql
+++
@@ -1,6 +0,0 @@
-create table USERS(
-                      ID int not null AUTO_INCREMENT,
-                      NAME varchar(100) not null,
-                      STATUS int,
-                      PRIMARY KEY ( ID )
-);
